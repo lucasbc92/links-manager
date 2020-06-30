6c50f67337d87dd5028b09bca0e44fb84aa29ad3
lucasbc92 <lbc92@hotmail.com>
2020-06-29 22:13:18 -0300

    Account signup strong data to database

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -13,15 +13,43 @@ router.get('/sign-in', (request, response) => {
 })
 
 router.get('/sign-up', async (request, response) => {
-    const email = 'lbc92@hotmail.com';
-    const password = 'password';
+    const {email, password} = request.body;
+
+    return response.json({email, password});
 
     const hash = bcrypt.hashSync(password, saltRounds)
-
     const result = await Account.create({
         email,
         password: hash
     })
+
+    return response.json(result);
+})
+
+router.post('/sign-up', async (request, response) => {
+
+    const {email, password} = request.body;
+
+    console.log({email, password});
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+
+    let result;
+
+    try {
+        result = await Account.create({
+            email,
+            password: hash
+        });
+    } catch(err) {
+        if (err.name === 'SequelizeUniqueConstraintError') {
+            console.log(err.errors[0].message);
+            return response.json({
+                error: "Email is already registered."
+            });
+        }
+        return response.json(err);
+    }  
 
     return response.json(result);
 })
--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/auth', authController);
 
 db.sequelize.sync().then( () => {
