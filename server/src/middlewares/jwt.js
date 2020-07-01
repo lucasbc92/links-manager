ea24826c0e46ac40ae2b4280f0cbc962aa5e6673
lucasbc92 <lbc92@hotmail.com>
2020-06-30 23:00:40 -0300

    Excluded auth paths from JWT

--- server/src/middlewares/jwt.js
+++ server/src/middlewares/jwt.js
@@ -1,6 +1,17 @@
 const { verifyJwt } = require('../helpers/jwt')
 
 const checkJwt = (request, response, next) => {
+
+    const { url: path } = request; //alias
+    
+    const excludedPaths = [
+        '/auth/sign-in',
+        '/auth/sign-up',
+    ];
+
+    const isExcluded = !!excludedPaths.find(p => p.startsWith(path)); //transforma o valor para booleano
+    if(isExcluded) return next(); //ignora a verificação de token dos excludedPaths
+
     let token = request.headers['authorization'];
     token = token ? token.slice(7,token.length) : null;
     //para eliminar o 'Bearer ' da string de token
